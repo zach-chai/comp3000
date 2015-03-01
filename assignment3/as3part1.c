ea363d3efbb81ff7e3a37c7b445ffb2771b07fe4
Zachary Chai <zachary.chai@younility.com>
2015-03-01 16:59:39 -0500

    part of part1 assignment3

--- assignment3/as3part1.c
+++ assignment3/as3part1.c
@@ -3,55 +3,154 @@
 #include <string.h>
 #include <pthread.h>
 
-#define STR_LEN 100
+
+#define STR_LEN 16 * 1024
+#define WORD_LEN 128
+
+typedef struct
+{
+  char word[WORD_LEN];
+  int count;
+} WORD;
+
+typedef struct _NODE NODE;
+
+struct _NODE
+{
+  NODE* next;
+  WORD* data;
+};
+
 char g_text[STR_LEN];
+NODE* head;
 
-void print_reverse(void *arg) {
+void insert (char* buff)
+{
+  NODE* curr_node = head;
 
-  int i;
-  int beg_word = STR_LEN;
-
-  char    tmp[STR_LEN];
-  char *  orig_str = (char *) arg;
-
-  for (i=STR_LEN - 1; i >= 0; i--)
+  while(1)
   {
-    /* COPY INDEX */
-    tmp[i] = orig_str[i];
-
-    if (' ' == orig_str[i])
-    /* IF SPACE, PRINT */
+    if (curr_node->data != NULL && strcmp(curr_node->data->word, buff) == 0)
     {
-      printf ("%s", tmp[i]);
-      memset (tmp, '\0', (int)STR_LEN);
+      ++(curr_node->data->count);
+      return;
     }
+    if(curr_node->next == NULL)
+      break;
+    curr_node = curr_node->next;
+  }
+
+
+  curr_node->next = (NODE *) malloc(sizeof(NODE));
+  curr_node = curr_node->next;
+
+  curr_node->data = (WORD *) malloc(sizeof(WORD));
+  strcpy(curr_node->data->word, buff);
+  curr_node->data->count = 0;
+  curr_node->next = NULL;
+}
+
+void print_reverse ()
+{
+  int i;
+  int size = 0;
+  int str_size = 0;
+  char tmp[WORD_LEN];
+  char* orig_str = g_text;
+
+  for(i=0; i < STR_LEN; i++)
+  {
+    if('\0' == orig_str[i])
+      break;
+    ++str_size;
+  }
+
+  int end_word = str_size - 2;
+
+  for (i=str_size - 1; i >= 0; i--)
+  {
+    if (' ' == orig_str[i])
+    {
+      // buff_size = 0;
+      // memset (tmp, '\0', (int)WORD_LEN);
+
+      size = end_word - i;
+      printf("%.*s", size, &orig_str[i+1]);
+      if(end_word == str_size - 2)
+        printf("%s", " ");
+      end_word = i;
+    }
+
+    // tmp[buff_size] = orig_str[i];
+    // ++buff_size;
+  }
+  printf("%.*s\n", end_word, orig_str);
+}
+
+void store_word_count ()
+{
+  int i;
+  char* orig_str = g_text;
+  char buffer[WORD_LEN];
+  int buff_size = 0;
+
+  memset (buffer, '\0', (int)WORD_LEN);
+
+  for (i=0; i < STR_LEN; i++)
+  {
+    if (' ' == orig_str[i] || '\0' == orig_str[i])
+    {
+      if(buff_size != 0)
+        insert(buffer);
+
+      buff_size = 0;
+      memset (buffer, '\0', (int)WORD_LEN);
+
+      if('\0' == orig_str[i])
+      {
+        break;
+      }
+      continue;
+    }
+
+    buffer[buff_size] = orig_str[i];
+    ++buff_size;
   }
 }
 
-void store_word_count(void *arg) {
+void append_to_file (void *arg, int len)
+{
 
 }
 
-void append_to_file(void *arg) {
+int main()
+{
 
-}
-
-int main() {
-
-  pthread_t t1, t2;
-  char str[STR_LEN];
+  pthread_t t1, t2, t3;
   int i;
 
+  head = (NODE *) malloc(sizeof(NODE));
+  head->data = NULL;
+
   printf( "Enter a line of text:\n");
-  fgets(str, 100, stdin);
+  fgets(g_text, STR_LEN, stdin);
 
-  printf("%s", str);
-
-  if (strcmp("exit", str) == 0) {
+  if (strcmp("exit", g_text) == 0) {
     return 0;
   }
 
-  print_reverse((void*)str);
+  print_reverse();
+
+  // store_word_count();
+
+  // NODE* curr_node = head->next;
+  // while(1)
+  // {
+  //   printf("%s\n", curr_node->data->word);
+  //   if(curr_node->next == NULL)
+  //     break;
+  //   curr_node = curr_node->next;
+  // }
 
   return 0;
 }
